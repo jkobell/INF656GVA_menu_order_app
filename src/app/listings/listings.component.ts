db7eac62862a041b7c9b95efaa12c62ee03a95a1
Kobell <k4kobell@gmail.com>
2023-10-08 19:25:18 -0500

    commit_listing_image_base64_draft0

--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Listing } from './listing';
 import { LISTINGS } from './mock_listings';
+import { LISTINGS_IMAGES } from './mock_listings_images';
 
 @Component({
     selector: 'app_listings',
@@ -11,6 +12,9 @@ import { LISTINGS } from './mock_listings';
   export class ListingsComponent {
     listings = LISTINGS;
     selectedListing?: Listing;
+
+    listings_images = LISTINGS_IMAGES;
+    image = this.listings_images[0].image_b64; 
 
     onSelect(listing: Listing): void {
         this.selectedListing = listing;
---
+++ src/app/listings/listings_image.ts
@@ -0,0 +1,5 @@
+export interface ListingsImage {
+    image_id: number;
+    image_name: string;
+    image_b64: string;
+}
